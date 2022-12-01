d1784b36ee6b2814a0ca25abfae466cf545f5220
Aaron Sandgren <aaron.sandgren@live.se>
2022-12-01 07:11:47 +0100

    Added autopull functionality of input to newday.py and added .gitignore to not push my cookie to git :)

--- 2022/newday.py
+++ 2022/newday.py
@@ -1,14 +1,19 @@
 import os
-
 from datetime import date
+from requests import get
 
 daynum = str(date.today())[-2:]
 if daynum[0] == '0': daynum = daynum[1:2]
 newpath = f"./day{daynum}"
+
 os.mkdir(newpath)
 
 open(f"{newpath}/solve.py", "x").write(
     "data = []\n\nfile = open('input.txt', 'r')\n\nfor line in file.readlines():\n\tdata.append(line)"
 )
 
-open(f"{newpath}/input.txt", "x")
+cookie = open(f"./session.env", "r").readlines()[0]
+
+input = get(f'https://adventofcode.com/2022/day/{daynum}/input', headers={'cookie': cookie})
+
+open(f"{newpath}/input.txt", "x").write(input.content.decode('utf-8'))
