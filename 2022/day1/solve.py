1b73470c17d0ddb3df87513e0e31486b9ff62c40
Aaron Sandgren <aaron.sandgren@live.se>
2022-12-01 13:21:49 +0100

    quick update

--- 2022/day1/solve.py
+++ 2022/day1/solve.py
@@ -17,32 +17,6 @@ for elem in data:
     else:
         cnt += elem
 
-maxi = 0
-max1 = 0
-
-for i, elem in enumerate(cals):
-    if elem > max1:
-        max1 = elem
-        maxi = i
-print(f"Answer 1: {max1}")
-cals.pop(maxi)
-
-maxi = 0
-max2 = 0
-
-for i, elem in enumerate(cals):
-    if elem > max2:
-        max2 = elem
-        maxi = i
-
-cals.pop(maxi)
-
-maxi = 0
-max3 = 3
-
-for i, elem in enumerate(cals):
-    if elem > max3:
-        max3 = elem
-        maxi = i
-
-print(f"Answer 2: {max1 + max2 +max3}") 
+cals = sorted(cals, reverse=True)
+print(f"Answer 1: {cals[0]}") 
+print(f"Answer 2: {sum(cals[0:3])}") 
