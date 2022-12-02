a51f6a556d85d474f46768642afbf1a520d9dd13
Aaron Sandgren <aaron.sandgren@live.se>
2022-12-02 06:32:54 +0100

    revised day2

--- 2022/day2/solve.py
+++ 2022/day2/solve.py
@@ -10,52 +10,46 @@ for line in file.readlines():
 	if line[2:3] == 'X': newline.append(1)
 	if line[2:3] == 'Y': newline.append(2)
 	if line[2:3] == 'Z': newline.append(3)
-	data.append(line)
+	data.append(newline)
 
+print(data[0])
 
-def vscore(abc, xyz) -> int:
-	if abc == xyz: return 3 # draw
-	if abc == 1 and xyz == 3: return 0 # rock vs paper lose
-	if abc == 1 and xyz == 2: return 6 # rock vs scissor win
-	if abc == 2 and xyz == 3: return 0 # paper vs scissor lose
-	if abc == 2 and xyz == 1: return 6 # paper vs rock win
-	if abc == 3 and xyz == 1: return 0 # scissor vs rock lose
-	if abc == 3 and xyz == 2: return 6 # scissor vs paper win
+def pickscore(elves, me) -> int:
+	if elves == me: return 3 # draw
+	if elves == 1 and me == 3: return 0 # rock vs scissor lose
+	if elves == 1 and me == 2: return 6 # rock vs paper win
+	if elves == 2 and me == 1: return 0 # paper vs rock lose
+	if elves == 2 and me == 3: return 6 # paper vs scissor win
+	if elves == 3 and me == 2: return 0 # scissor vs paper lose
+	if elves == 3 and me == 1: return 6 # scissor vs rock win
 
 score = 0
 for line in data:
 	elves = line[0]
 	me = line [1]
-	score += vscore(elves, me) + me
+	score += pickscore(elves, me) + me
 
 
 print(f"Part 1: {score}")
 
-# score2 = 0
 
-# for line in data:
-# 	elves = line[0:1]
-# 	outcome = line [2:3]
-# 	if elves == 'A':
-# 		if outcome == 'X':
-# 			score2 += 3 # pick scissor + lose
-# 		if outcome == 'Y':
-# 			score2 += 4 # pick rock + draw
-# 		if outcome == 'Z':
-# 			score2 += 8 # pick paper + win
-# 	if elves == 'B':
-# 		if outcome == 'X':
-# 			score2 += 1 # pick rock + lose
-# 		if outcome == 'Y':
-# 			score2 += 5 # pick paper + draw
-# 		if outcome == 'Z':
-# 			score2 += 9 # pick scissors + win	
-# 	if elves == 'C':
-# 		if outcome == 'X':
-# 			score2 += 2 # pick paper + lose
-# 		if outcome == 'Y':
-# 			score2 += 6 # pick Scissors + draw
-# 		if outcome == 'Z':
-# 			score2 += 7 # pick rock + win				
+def outcomescore(elves, outcome) -> int:
+	if outcome == 1:
+		if elves == 1: return 3 # elves rock lose
+		if elves == 2: return 1 # elves paper lose
+		if elves == 3: return 2 # elves scissor lose
+	if outcome == 2:
+		return 3 + elves
+	if outcome == 3:
+		if elves == 1: return 2 + 6 # elves rock win
+		if elves == 2: return 3 + 6 # elves paper win
+		if elves == 3: return 1 + 6 # elves scissor win
+	
+score2 = 0
 
-# print(f"Part 2: {score2}")
+for line in data:
+	elves = line[0]
+	me = line [1]
+	score2 += outcomescore(elves, me)
+
+print(f"Part 2: {score2}")
