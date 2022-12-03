d3100566788cf9cf3d8e6a360df7bb074dab78f2
Aaron Sandgren <aaron.sandgren@live.se>
2022-12-03 07:03:55 +0100

    day3 cleanup

--- 2022/day3/solve.py
+++ 2022/day3/solve.py
@@ -1,46 +1,39 @@
 data = []
+data2 = []
 
 file = open('input.txt', 'r')
 
 for line in file.readlines():
 	line = line.strip("\n") 
+	data2.append(line)
 	tmp = []
-	tmp.append(line[0:int((len(line)/2))])
-	tmp.append(line[int((len(line)/2)):])
+	tmp.append(line[:len(line)//2])
+	tmp.append(line[len(line)//2:])
 	data.append(tmp)
+
+def getletterval(letter: str) -> int:
+	letter = ord(letter)
+	if letter >= 97:
+		return letter - 96
+	else:
+		return letter - 64 + 26
 
 count = 0
 for line in data:
-	inboth = ''
 	for letter in line[0]:
 		if letter in list(line[1]):
-			inboth = letter
+			count += getletterval(letter)
 			break
-	val = ord(inboth)
-	if val >= 97:
-		count += val -96
-	else:
-		count += val - 64 + 26
-
 print(f"Part 1:{count}")
-count = 0
 
+count2 = 0
 for i in range(0, len(data), 3):
-	str1 = data[i][0] + data[i][1]
-	str2 = data[i+1][0] + data[i+1][1]
-	str3 = data[i+2][0] + data[i+2][1]
-	inboth = ''
-	for letter in str1:
-		if letter in str2 and letter in str3:
-			inboth = letter
+	for letter in data2[i]:
+		if letter in data2[i+1] and letter in data2[i+2]:
+			count2 += getletterval(letter)
 			break
-	val = ord(inboth)
-	if val >= 97:
-		count += val -96
-	else:
-		count += val - 64 + 26
+print(f"Part 2:{count2}")
 
-print(f"Part 2:{count}")
 
 
 
