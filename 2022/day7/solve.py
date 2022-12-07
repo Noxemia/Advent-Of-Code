709bb3bfbc50b57ae16be9d350dfdc10fa3ab92b
Aaron Sandgren <aaron.sandgren@live.se>
2022-12-07 07:06:10 +0100

    idk

--- 2022/day7/solve.py
+++ 2022/day7/solve.py
@@ -8,56 +8,26 @@ data = []
 for line in open('input.txt', 'r').readlines():
 	data.append(line.strip("\n"))
 
-def def_value():
-	return [0,[]]
-#dir = [size, [files], [dirs],recsize]
+@dataclass
 
-fs = {}
+@dataclass
+class node:
+	parent: str
+	name: str
+	files: list
+	filessize: int
+	childs: list
+	childssize: int
 
-currentdirectory = ""
+	def __init__(self, parent, name):
+		self.name = name
+		self.parent = parent
+		self.files = []
+		self.filessize = 0
+		self.childs = []
+		self.childssize = 0
 
-for line in data:
-	line = line.split(" ")
-	if line[0] == "$":
-		if line[1] == "cd": 
-			if line[2] == "..":
-				continue
-			else: 
-				currentdirectory = line[2]
-				if not currentdirectory in fs.keys():
-					fs[currentdirectory] = [0,[],[],0]
-		# ls
-		else: continue
 	
-	elif line[0] == "dir":
-		if not line[1] in fs[currentdirectory][2]:
-			fs[currentdirectory][2].append(line[1])
-	else:
-		if not line[1] in fs[currentdirectory][1]:
-			fs[currentdirectory][1].append(line[1])
-			fs[currentdirectory][0] += int(line[0])
+root = node(None, "/")
 
-
-def dir_size(dirname) -> int:
-	size = 0
-	size += fs[dirname][0]
-	for nextdir in fs[dirname][2]:
-		if size > 100000: return 100001
-		size += dir_size(nextdir)
-	if size > 100000: return 100001
-	return size
-
-
-for dir in fs.keys():
-	size = dir_size(dir)
-	print(dir)
-	fs[dir][3] = size
-
-p1cnt = 0
-for dir in fs.keys():
-	if fs[dir][3] <= 100000:
-		p1cnt += fs[dir][3]
-
-print(fs)
-print(p1cnt)
 
