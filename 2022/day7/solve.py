1c208c6bc3fa1d5500ffec2691b85578c7823095
Aaron Sandgren <aaron.sandgren@live.se>
2022-12-07 09:34:43 +0100

    day 7 cleanup

--- 2022/day7/solve.py
+++ 2022/day7/solve.py
@@ -1,33 +1,67 @@
-
-from collections import defaultdict
 from dataclasses import dataclass
-import sys
-sys.setrecursionlimit(20000)
 data = []
 
 for line in open('input.txt', 'r').readlines():
 	data.append(line.strip("\n"))
 
 @dataclass
-
-@dataclass
-class node:
-	parent: str
-	name: str
-	files: list
+class Node:
+	parentdir: None
+	dirname: str
 	filessize: int
-	childs: list
-	childssize: int
-
-	def __init__(self, parent, name):
-		self.name = name
-		self.parent = parent
-		self.files = []
+	directories: dict
+	def __init__(self, parent, name:str):
+		self.dirname = name
+		self.parentdir = parent
 		self.filessize = 0
-		self.childs = []
-		self.childssize = 0
+		self.directories = {}
 
-	
-root = node(None, "/")
+root = Node(None, "/")
 
+currentdir = root
+data.pop(0)
+allnodes = [root]
+for line in data:
+	line = line.split()
+	# command
+	if line[0] == "$":
+		# cd
+		if line [1] == "cd":
+			if line[2] == "..":
+				currentdir = currentdir.parentdir
+			else:
+				currentdir = currentdir.directories[line[2]]
+		# ls has no impact
+	# dir file
+	elif line[0] == "dir":
+		newnode = Node(currentdir, line[1])
+		allnodes.append(newnode)
+		currentdir.directories[line[1]] = newnode
+	# regular file
+	else:
+		currentdir.filessize += int(line[0])
 
+def findsize(node: Node) -> int:
+	size = node.filessize
+	for dir in node.directories.keys():
+		size += findsize(node.directories[dir])
+	return size
+
+p1cnt = 0
+for node in allnodes:
+	val = findsize(node)
+	if val <= 100000:
+		p1cnt += val
+
+print(f"Part 1: {p1cnt}")
+
+rootsize = findsize(root)
+mindeletesize = 30000000 - (70000000 - rootsize)
+
+currentmin = 70000000
+for node in allnodes:
+	val = findsize(node)
+	if val > mindeletesize and val < currentmin:
+		currentmin = val
+
+print(f"Part 2: {currentmin}")
