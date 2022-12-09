f5b0c566fbbeb4eaabdfa4bf2791128dd4b27c7f
Aaron Sandgren <aaron.sandgren@live.se>
2022-12-09 18:19:12 +0100

    cleanup 2

--- 2022/day9/solve.py
+++ 2022/day9/solve.py
@@ -5,13 +5,8 @@ for line in open('input.txt', 'r').readlines():
 	spl = line.split(" ")
 	data.append([spl[0], int(spl[1])])
 
-@dataclass
-class vars:
-	tailx: list
-	taily: list
-
 def touching(x1,y1, x2,y2) -> bool:
-	#tail is one above
+	# tail is one above
 	if y1 +1 == y2:
 		if x1 == x2: return True
 		if x1-1 == x2: return True
@@ -28,66 +23,51 @@ def touching(x1,y1, x2,y2) -> bool:
 		if x1+1 == x2: return True
 	return False
 
-
-def moveright():
-	var.tailx[0] = var.tailx[0] + 1
-
-def moveleft():
-	var.tailx[0] = var.tailx[0] - 1
-
-def moveup():
-	var.taily[0] = var.taily[0] + 1
-
-def movedown():
-	var.taily[0] = var.taily[0] - 1
-
 def updatetail():
-	taillen = len(var.tailx)
+	taillen = len(tailx)
 	for i in range(1,taillen):
-		if not touching(var.tailx[i-1],var.taily[i-1],var.tailx[i],var.taily[i]):
+		if not touching(tailx[i-1],taily[i-1],tailx[i],taily[i]):
 			# same col move
-			if var.tailx[i-1] == var.tailx[i]:
-				if var.taily[i-1] > var.taily[i]: var.taily[i] += 1
-				if var.taily[i-1] < var.taily[i]: var.taily[i] -= 1
+			if tailx[i-1] == tailx[i]:
+				if taily[i-1] > taily[i]: taily[i] += 1
+				if taily[i-1] < taily[i]: taily[i] -= 1
 			# same row move
-			elif var.taily[i-1] == var.taily[i]:
-				if var.tailx[i-1] > var.tailx[i]: var.tailx[i] += 1
-				if var.tailx[i-1] < var.tailx[i]: var.tailx[i] -= 1
+			elif taily[i-1] == taily[i]:
+				if tailx[i-1] > tailx[i]: tailx[i] += 1
+				if tailx[i-1] < tailx[i]: tailx[i] -= 1
 			#diag move
 			else:
-				if var.taily[i-1] > var.taily[i]: var.taily[i] += 1
-				if var.taily[i-1] < var.taily[i]: var.taily[i] -= 1
-				if var.tailx[i-1] > var.tailx[i]: var.tailx[i] += 1
-				if var.tailx[i-1] < var.tailx[i]: var.tailx[i] -= 1
+				if taily[i-1] > taily[i]: taily[i] += 1
+				if taily[i-1] < taily[i]: taily[i] -= 1
+				if tailx[i-1] > tailx[i]: tailx[i] += 1
+				if tailx[i-1] < tailx[i]: tailx[i] -= 1
 		else:
 			break
-	coords = [var.tailx[taillen-1], var.taily[taillen-1]]
+	coords = [tailx[taillen-1], taily[taillen-1]]
 	if not coords in scorecoords:
 		scorecoords.append(coords)
 
-
-
 def calc(taillengt: int):
-	global var 
-	global scorecoords
-	var = vars([0]*taillengt, [0]*taillengt)
+	global tailx, taily, scorecoords
+	tailx = [0]*taillengt
+	taily = [0]*taillengt
 	scorecoords = [[0,0]]
 	for ins in data:
 		if ins[0] == "R":
 			for _ in range(ins[1]):
-				moveright()
+				tailx[0] = tailx[0] + 1
 				updatetail()
 		if ins[0] == "L":
 			for _ in range(ins[1]):
-				moveleft()
+				tailx[0] = tailx[0] - 1
 				updatetail()
 		if ins[0] == "U":
 			for _ in range(ins[1]):
-				moveup()
+				taily[0] = taily[0] + 1
 				updatetail()
 		if ins[0] == "D":
 			for _ in range(ins[1]):
-				movedown()
+				taily[0] = taily[0] - 1
 				updatetail()
 	print(len(scorecoords))
 calc(2)
