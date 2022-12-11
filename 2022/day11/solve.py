364dbb2f9780582c64d42cf2fc8c3c0c81700575
Aaron Sandgren <aaron.sandgren@live.se>
2022-12-11 21:42:20 +0100

    day 11 cleanup

--- 2022/day11/solve.py
+++ 2022/day11/solve.py
@@ -1,77 +1,86 @@
 from dataclasses import dataclass
+from copy import deepcopy
 
 @dataclass
 class Monkey:
-	index: int
 	items: list
-	fun: classmethod
-	operator: str
-	oparg: int
-	testdiv: int
 	ontrue: int
 	onfalse: int
 	inspectcount: int
+	operation: classmethod
+	testdiv: classmethod
 
-monkeys = []
+totmod: int = 0
+monkeys: list[Monkey] = []
+tmp: list[str] = []
 
-tmp = []
+def parsetmp():
+	global totmod
+	items: list[int] = [int(x.strip(",")) for x in tmp[1].split(" ")[4:]]
+	tmpoparg: str = tmp[2].split(" ")[-1]
+	tmpoperator: str = tmp[2].split(" ")[-2]
+	mod = int(tmp[3].split(" ")[-1])
+	totmod = mod if totmod == 0 else totmod*mod
+	ontrue = int(tmp[4].split(" ")[-1])
+	onfalse = int(tmp[5].split(" ")[-1])
+	testdiv =  lambda x: x % mod == 0
+	operation = None
+	if tmpoparg == "old":
+		operation = lambda x: x*x
+	else:
+		if tmpoperator == "+":
+			operation = lambda x: x + int(tmpoparg)
+		else:
+			operation = lambda x: x * int(tmpoparg)
+	monkey = Monkey(items, ontrue, onfalse, 0, operation, testdiv)
+	return monkey
+
 for line in open('input.txt', 'r').readlines():
 	if line == "\n": continue
-	tmp.append(line)
+	tmp.append(line.strip("\n"))
 	if len(tmp) == 6:
-		index = int(tmp[0].split(" ")[1].strip(":\n"))
-		items = [int(x.strip(",")) for x in tmp[1].split(" ")[4:]]
-		opop = tmp[2].split(" ")[-2]
-		tmpoparg = tmp[2].split(" ")[-1]
-		oparg = None
-		if tmpoparg[0] == "o":
-			oparg = 0
-		else:
-			oparg = int(tmpoparg)
-		testdiv = int(tmp[3].split(" ")[-1])
-		ontrue = int(tmp[4].split(" ")[-1])
-		onfalse = int(tmp[5].split(" ")[-1])
-		monkeys.append(Monkey(index, items, opop, oparg, testdiv, ontrue, onfalse, 0))
+		monkeys.append(parsetmp())
 		tmp = []
+		
 
-def inspects(monkey: Monkey):
+	
+def inspects(monkey: Monkey, p1: bool):
 	items = monkey.items
 	for item in items:
 		monkey.inspectcount += 1
-		worry = 0
-		if monkey.oparg == 0:
-			worry = item*item
+		worry = monkey.operation(item)
+		if p1: worry = worry//3
+		if monkey.testdiv(worry):
+			monkeys[monkey.ontrue].items.append(worry)
 		else:
-			if monkey.operator == "+":
-				worry = item + monkey.oparg
-			else:
-				worry = item * monkey.oparg
-		newworry = worry
-		if newworry % monkey.testdiv == 0:
-			monkeys[monkey.ontrue].items.append(newworry)
-		else:
-			monkeys[monkey.onfalse].items.append(newworry)
+			monkeys[monkey.onfalse].items.append(worry)
 	monkey.items = []
-
-
-mod = monkeys[0].testdiv
-for monkey in monkeys[1:]:
-	mod *= monkey.testdiv
 
 def minimizeworry():
 	for monkey in monkeys:
-		monkey.items = [x % mod for x in monkey.items]
+		monkey.items = [x % totmod for x in monkey.items]
 
-def round():
+def round(p1: bool):
 	for monkey in monkeys:
-		inspects(monkey)
+		inspects(monkey, p1)
 	minimizeworry()
 
 def sorting(monkey: Monkey):
 	return monkey.inspectcount
 
-for _ in range(10000 ): round()
-monkeys = sorted(monkeys, key=sorting, reverse=True)
 
+def p1():
+	global monkeys
+	ogmonkeys = deepcopy(monkeys)
+	for _ in range(20): round(True)
+	monkeys = sorted(monkeys, key=sorting, reverse=True)
+	print(f"Part 1: {monkeys[0].inspectcount * monkeys[1].inspectcount}")
+	monkeys = ogmonkeys
 
-print(monkeys[0].inspectcount * monkeys[1].inspectcount)
+def p2():
+	global monkeys
+	for _ in range(10000): round(False)
+	monkeys = sorted(monkeys, key=sorting, reverse=True)
+	print(f"Part 2: {monkeys[0].inspectcount * monkeys[1].inspectcount}")
+p1()
+p2()
