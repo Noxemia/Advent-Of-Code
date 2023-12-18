84ed15a0c26b4ae48f531837c526fc19be203c46
Aaron Sandgren <aaron.sandgren@live.se>
2023-12-18 07:13:27 +0100

    day 17 finished

--- 2023/day17/solve.py
+++ 2023/day17/solve.py
@@ -1,92 +1,38 @@
-from typing import List, Tuple
-from enum import Enum
-from dataclasses import dataclass
-from copy import deepcopy
 from heapq import heappop, heappush
-
-class Dir(Enum):
-	R = ">"
-	L = "<"
-	U = "^"
-	D = "v"
-	S = "s"
-
-@dataclass
-class Instr():
-	idx: int
-	idy: int
-	cost: int
-	TotDist: int #totaldistance
-	LastDirs: List[Dir] # 3 lasts directions
 
 data = []
 
 for line in open('input.txt', 'r').readlines():
 	data.append([int(x) for x in list(line.strip())])
 
-visited = {}
-Nexts: List[Instr] = []
+## (idx, idy, direction, dirTime)
+visited = set()
+## (TotDistance, idx, idy, cost, direction, dirTime)
+queue = [(0,0,0,0,0,0)]
 
-def getCoords(x:int,y:int, disallowedDir: Dir, backTrack: Dir) -> List[Tuple[int, int, Dir]]:
-	res = []
-	if x != 0 and disallowedDir != Dir.L and backTrack != Dir.R:
-		res.append((x-1, y, Dir.L))
-	if x != len(data[0]) -1 and disallowedDir != Dir.R and backTrack != Dir.L:
-		res.append((x+1, y, Dir.R))
-	if y != 0 and disallowedDir != Dir.U and backTrack != Dir.D:
-		res.append((x, y-1, Dir.U))
-	if y != len(data)-1 and disallowedDir != Dir.D and backTrack != Dir.U:
-		res.append((x, y+1, Dir.D))
-	return res
-
-def sortNexts():
-	global Nexts
-	Nexts = sorted(Nexts, key=lambda x: x.TotDist)
-
-#Nexts.append(Instr(0, 0, 0, 0, [Dir.R]))
-Nexts.append(Instr(0, 0, 0, 0, [Dir.S]))
-ans: Instr = None
-while len(Nexts) != 0:
-	instr: Instr = Nexts.pop(0)
-	newDist = instr.TotDist + instr.cost
-	if newDist > 1500: continue
-	## Check that we have not been in the same state before
-	if len(instr.LastDirs) == 3:
-		currentDirection = instr.LastDirs[2]
-		walkLenght = 1
-		if instr.LastDirs[1] == currentDirection:
-			walkLenght += 1
-			if instr.LastDirs[0] == currentDirection:
-				walkLenght += 1
-
-
-		if (instr.idx, instr.idy, currentDirection, walkLenght) in visited: continue
-		visited[(instr.idx, instr.idy, currentDirection, walkLenght)] = 0
-
-	## Check if we are done
-	if instr.idx == (len(data[0])-1) and instr.idy == (len(data)-1):
-		print(instr.TotDist+instr.cost)
-		ans = instr
+while len(queue) != 0:
+	dist, idx, idy, cost, direction, dirTime = heappop(queue)
+	if idx == len(data[0])-1 and idy == len(data)-1:
+		print("Done:", dist + cost)
 		break
+	#print("Current: ", dist, idx, idy, cost, direction, dirTime)
+	if (idx, idy, direction, dirTime) in visited: continue
+	visited.add((idx, idy, direction, dirTime))
 
-
-	## Check that we have not gone in the same direction 3 times
-	canNotWalkSameDir = all([node == instr.LastDirs[0] for node in instr.LastDirs]) and len(instr.LastDirs) >= 3 
-	disallowedDir = None
-	if canNotWalkSameDir:
-		disallowedDir = instr.LastDirs[0]
-
-	nextCoords = getCoords(instr.idx, instr.idy, disallowedDir, instr.LastDirs[-1])
-
-	for (idx, idy, _dir) in nextCoords:
-		dirCopy = deepcopy(instr.LastDirs)
-		newDir = []
-		for x in dirCopy: newDir.append(x)
-		newDir.append(_dir)
-		if len(newDir) > 3: newDir.pop(0)
-		
-		Nexts.append(Instr(idx, idy, data[idy][idx], newDist, newDir))
-	#print(len(Nexts))
-	sortNexts()
-
-print("?")
+	directions = [(0,1), (1,0), (0, -1), (-1, 0)]
+	
+	if dirTime > 0 and dirTime < 4:
+		newx = idx + directions[direction][0]
+		newy = idy + directions[direction][1]
+		if newx < 0 or newx > len(data[0])-1 or newy < 0 or newy > len(data)-1: continue
+		heappush(queue, (dist+cost, newx, newy, data[newy][newx], direction, dirTime+1))
+		continue
+	else:
+		for _dir, mod in enumerate(directions): # down right up left, 0,1,2,3
+			if abs(_dir - direction) == 2 and dirTime != 0: continue
+			newx = idx + mod[0]
+			newy = idy + mod[1]
+			if newx < 0 or newx > len(data[0])-1 or newy < 0 or newy > len(data)-1: continue
+			newDirTime = dirTime + 1 if direction == _dir else 1
+			if newDirTime > 10: continue
+			heappush(queue, (dist+cost, newx, newy, data[newy][newx], _dir, newDirTime))
