7b0f5b4e4a34662162a756ec96b1037475d0b960
Aaron Sandgren <aaron.sandgren@live.se>
2023-12-18 17:55:08 +0100

    day 18 finished

--- 2023/day18/solve.py
+++ 2023/day18/solve.py
@@ -1,88 +1,96 @@
+from collections import defaultdict
 data = []
 
 for line in open('input.txt', 'r').readlines():
 	line = line.strip()
 	direction = line.split(" ")[0]
 	amount = line.split(" ")[1]
-	data.append((direction, int(amount)))
+	#data.append((direction, int(amount)))
+	# hexconversion
+	hexstr = line.split(" ")[2][2:-1]
+	hamount = int(hexstr[:-1], 16)
+	_dirs = ["R", "D", "L", "U"]
+	hdirection = _dirs[int(hexstr[-1])]
+	data.append((hdirection, hamount))
 
-
-#print(data)
 curx = 0
 cury = 0
-minx = 0
-maxx = 0
-miny = 0
-maxy = 0
 
-finalCoords = [(0,0)]
-cnt = 0
+rowSpans = defaultdict(list)
+
 for (direction, amount) in data:
-	#if cnt > 7: break
-	cnt += 1
 	if direction == "R":
-		for newx in range(1+curx, amount+1+curx):
-			curx = newx
-			minx = min(minx, curx)
-			maxx = max(maxx, curx)
-			finalCoords.append((curx, cury))
+		spanStart = curx
+		spanEnd = curx+amount
+		curx = spanEnd
+		rowSpans[cury].append((spanStart,spanEnd))
+
 	if direction == "L":
-		for newx in range(curx, curx-amount-1, -1):
-			curx = newx
-			minx = min(minx, curx)
-			maxx = max(maxx, curx)
-			finalCoords.append((curx, cury))
+		spanStart = curx
+		spanEnd = curx-amount
+		curx = spanEnd
+		rowSpans[cury].append((spanEnd, spanStart))
 	if direction == "D":
-		for newy in range(1+cury, amount+1+cury):
-			cury = newy
-			miny = min(miny, cury)
-			maxy = max(maxy, cury)
-			finalCoords.append((curx, cury))
+		for newy in range(cury+1, amount+cury):
+			rowSpans[newy].append((curx,curx))
+		cury = newy+1
 	if direction == "U":
-		for newy in range(cury, cury-amount-1, -1):
-			cury = newy
-			miny = min(miny, cury)
-			maxy = max(maxy, cury)
-			finalCoords.append((curx, cury))
+		for newy in range(cury-1, cury-amount, -1):
+			rowSpans[newy].append((curx,curx))
+		cury = newy-1
+		
+def isTrueEdge(x1, x2, y):
+	#toprow
+	if y-1 in rowSpans:
+		x1in = False
+		x2in = False
+		for (sx1, sx2) in rowSpans[y-1]:
+			if sx1 <= x1 <=sx2: x1in = True
+			if sx1 <= x2 <=sx2: x2in = True
+		
+		if x1in and x2in: return True
 
-digMap = []
-for _ in range(maxy+1+abs(miny)):
-	digMap.append(["."]*(maxx+1+abs(minx)))
+	# bottomrow
+	if y+1 in rowSpans:
+		
+		x1in2 = False
+		x2in2 = False
+		for (sx1, sx2) in rowSpans[y+1]:
+			if sx1 <= x1 <= sx2: x1in2 = True
+			if sx1 <= x2 <= sx2: x2in2 = True
+		if x1in2 and x2in2: return True 
+	return False
 
-for (x,y) in finalCoords:
-	digMap[y+abs(miny)][x+abs(minx)] = "#"
-
-for line in digMap:
-	print(line)
-
-## flood fill
-
-visited = set()
-
-nexts = [(61,11)]
-
-while nexts:
-	idx, idy = nexts.pop()
-	digMap[idy][idx] = "x"
-	
-	directions = [(0,1), (1,0), (0, -1), (-1, 0)]
-	visited.add((idx, idy))
-
-	newCoords = []
-	for (dx, dy) in directions:
-		nx = idx + dx
-		ny = idy + dy
-		if digMap[ny][nx] == "#": continue
-		if (nx, ny) in visited: continue
-		nexts.append((nx,ny))
-
-print("#########")
-for line in digMap:
-	print(line)
 
 p1sum = 0
-for line in digMap:
-	for c in line:
-		if c != ".": p1sum += 1
+for key in sorted(rowSpans.keys()):
+	inner = False
+	innerStart = 0
+	for (x1, x2) in sorted(rowSpans[key], key=lambda x: x[0]):
+		# for a out side
+		
+		if x1 != x2:
+			p1sum += abs(x1 - x2) + 1
+			if not inner:
+				if not isTrueEdge(x1,x2, key):
+					inner = True
+					innerStart = x2+1
+			else:
+				if isTrueEdge(x1,x2, key):
+					p1sum += abs(innerStart - (x1-1)) +1
+					innerStart = x2+1
+				else:
+					inner = False
+					p1sum += abs(innerStart - (x1-1)) +1
+		
+		# if we are on an single piece
+		elif x1 == x2:
+			## if we outside of the shape going into it
+			if not inner:
+				inner = True
+				innerStart = x1
+			else:
+				inner = False
+				p1sum += abs(innerStart - x1) +1
 
-print(p1sum)
+print("Sum: ", p1sum)
