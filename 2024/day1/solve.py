7f7162606faad6d95fc98e8c4b14944280a322e3
Aaron Sandgren <aaron.sandgren@live.se>
2024-12-01 15:25:30 +0100

    Day 1 2024

--- 2024/day1/solve.py
+++ 2024/day1/solve.py
@@ -2,15 +2,15 @@ from collections import Counter
 left = []
 right = []
 for line in open('input.txt', 'r').readlines():
-    line = line.strip("\n").split("  ")
+    line = line.split("  ")
     left.append(int(line[0]))
     right.append(int(line[1]))
 
-tot1 = sum([abs(r-l) for l,r in zip(sorted(left), sorted(right))])
+tot1 = sum(abs(r-l) for l,r in zip(sorted(left), sorted(right)))
 
 print("Part 1:", tot1)
 
 rcount = Counter(right)
-tot2 = sum([l * rcount.get(l, 0) for l in left])
+tot2 = sum(l * rcount[l] for l in left)
 
 print("Part 2:", tot2)
