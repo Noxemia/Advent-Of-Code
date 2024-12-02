f4ba79088ceae91969d997c1e4a134eda8c2a563
Aaron Sandgren <aaron.sandgren@live.se>
2024-12-02 13:56:31 +0100

    Day 2 improve

--- 2024/day2/solve.py
+++ 2024/day2/solve.py
@@ -1,8 +1,7 @@
-from copy import copy
-data = [[int(x) for x in y] for y in (x.split(" ") for x in open('input.txt', 'r').readlines())]
+data = [[int(i) for i in x.split()] for x in open('input.txt', 'r').readlines()]
 
 def validateReport(report):   
-    if not (report == sorted(report) or report == sorted(report, reverse=True)): return False
+    if report != sorted(report) and report != sorted(report, reverse=True): return False
     for i in range(len(report)-1):
         if (x := report[i]) == (y := report[i+1]) or abs(x-y) > 3: return False
     return True
@@ -10,13 +9,10 @@ def validateReport(report):
 tot1, tot2 = 0, 0
 for report in data:
     if validateReport(report): 
-        tot1 += 1
-        tot2 += 1
+        tot1, tot2 = tot1 + 1, tot2 + 1
         continue
     for i in range(len(report)):
-        rc = copy(report)
-        rc.pop(i)
-        if validateReport(rc):
+        if validateReport(report[:i] + report[i+1:]):
             tot2 += 1
             break
 
