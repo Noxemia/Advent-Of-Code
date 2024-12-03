3231aab11c6f7fd6019afe64d39f7c94acd3a470
Aaron Sandgren <aaron.sandgren@live.se>
2024-12-03 08:18:12 +0100

    Both solutions and time

--- 2024/day3/solve.py
+++ 2024/day3/solve.py
@@ -1,9 +1,11 @@
 
 import re
+import time
 data = ""
 for line in open('input.txt', 'r').readlines():
     data += line
 
+st1 = time.time_ns()
 pattern = r"(mul\(\d+,\d+\))|(do\(\))|(don\'t\(\))"
 matches = re.findall(pattern, data)
 operations = []
@@ -23,5 +25,44 @@ for op in operations:
     else:
         part1 += op
         if enabled: part2 += op
+ed1 = time.time_ns()
+print("Part 1:", part1, "\nPart 2:", part2)
+
+data = [x for x in data]
+st2 = time.time_ns()
+
+part1, part2 = 0, 0
+enabled = True
+for i, c in enumerate(data):
+    dnt = "".join(data[i:i+7])
+    if dnt == "don't()":
+        enabled = False
+    do = "".join(data[i:i+4])
+    if do == "do()": 
+        enabled = True
+    try:
+        cons = "".join(data[i:i+4])
+        ## Check for mul(
+        if cons != "mul(": 
+            continue
+        ## look for and )
+        iparen = 0
+        for ii, cc in enumerate(data[i+4:i+13]):
+            if cc == ')':
+                iparen = i + ii + 4
+                break
+        if iparen == 0: continue
+        
+        nums = "".join(data[i+4:iparen])
+        nums = nums.split(",")
+        f = int(nums[0])
+        s = int(nums[1])
+        part1 += f*s
+        if enabled: 
+            part2 += f*s
+    except:
+        pass
+ed2 = time.time_ns()
 
 print("Part 1:", part1, "\nPart 2:", part2)
+print("Time regex: \t", ed1-st1, "\nTime ownparse\t", ed2-st2)
