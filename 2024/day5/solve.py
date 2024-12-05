2c961fdf16fa0bb7f24758e6ddfe09aa58bdb9d0
Aaron Sandgren <aaron.sandgren@live.se>
2024-12-05 16:05:25 +0100

    day 5 cleanup

--- 2024/day5/solve.py
+++ 2024/day5/solve.py
@@ -1,15 +1,13 @@
 from typing import Dict, List, Tuple
-rules: Dict[int, List[int]] = {}
+from collections import defaultdict
+rules: Dict[int, List[int]] = defaultdict(list)
 updates: List[List[int]] = []
 
 for line in open('input.txt', 'r').readlines():
     if line == "\n": continue
     elif "|" in line:
         v1, v2 = line.split("|")
-        if int(v1) in rules.keys():
-            rules[int(v1)].append(int(v2))
-        else:
-            rules[int(v1)] = [int(v2)]
+        rules[int(v1)].append(int(v2))
     else:
         updates.append(list(reversed([int(x) for x in line.split(",")])))
 
@@ -17,28 +15,24 @@ def tryOrder(update: List[int]) -> Tuple[bool, int, int]:
     for i, page in enumerate(update):
         if page not in rules: continue
         r = rules[page]
-        for y, next in enumerate(update[i+1:]):
+        for next in update[i+1:]:
             if next in r:
                 return (False, page, next)
     return (True, 0, 0)
 
-count = 0
+count, count2 = 0, 0 
 for update in updates:
-    if tryOrder(update)[0]: count += update[len(update)//2]
-
-count2 = 0
-for update in updates:
-    if tryOrder(update)[0]: continue
+    if tryOrder(update)[0]: 
+        count += update[len(update)//2]
+        continue
     correct, i, y = False, 0, 0
     while True:
         correct, i, y = tryOrder(update)
         if correct: 
             count2 += update[len(update)//2]
             break
-
-        ni = update.index(i)
-        ny = update.index(y)
-        update[ny] = i
-        update[ni] = y
+        
+        update[update.index(y)] = i
+        update[update.index(i)] = y
 
 print(count, count2)
