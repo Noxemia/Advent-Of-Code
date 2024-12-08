177cb916208582048cb476cf6fe26693570f2b0b
Aaron Sandgren <aaron.sandgren@live.se>
2024-12-08 01:13:29 +0100

    optimization day 7

--- 2024/day7/solve.py
+++ 2024/day7/solve.py
@@ -3,42 +3,52 @@ from enum import Enum
 from copy import copy
 
 class Op(Enum):
-   Add = "+"
-   Mul = "*"
-   Con = "||"
+    Add = "+"
+    Mul = "*"
+    Con = "||"
 
 eqs: List[Tuple[int, List[int]]] = []
-for line in open('input.txt', 'r').readlines():
-   line = line.split(":")
-   eqs.append((int(line[0]), [int(x) for x in line[1].split(" ")[1:]]))
+for line in open("input.txt", "r").readlines():
+    line = line.split(":")
+    eqs.append((int(line[0]), [int(x) for x in line[1].split(" ")[1:]]))
 
-operators = [Op.Add, Op.Mul]
-def recSolve(target: int, cval: int, rest: List[int], coperator: Op):
+operators = [Op.Add, Op.Mul, Op.Con]
+found = False
+gconUsed = False
+
+def recSolve(target: int, cval: int, rest: List[int], coperator: Op, conUsed: bool):
+    global found, gconUsed
+    if found: return False
     if cval > target: return False
-    ## add do current operator
+
+    # add do current operator
     if coperator == Op.Add:
-       cval += rest.pop(0)
+        cval += rest.pop(0)
     elif coperator == Op.Mul:
         cval *= rest.pop(0)
     elif coperator == Op.Con:
-       cval = int(str(cval)+str(rest.pop(0)))
+        conUsed = True
+        cval = int(str(cval) + str(rest.pop(0)))
 
-    ## Check if list is empty that we reached target
+    # Check if list is empty that we reached target
     if (len(rest)) == 0:
-       return True if cval == target else False
-    
-    return any(recSolve(target, cval, copy(rest), noperator) for noperator in operators)
+        if cval == target:
+            if conUsed:
+                gconUsed = True
+            found = True
+            return True
+        return False
+
+    return any(recSolve(target, cval, copy(rest), noperator, conUsed) for noperator in operators)
 
 part1 = 0
-for eq in eqs:
-    if any(recSolve(eq[0], 0, copy(eq[1]), nop) for nop in operators):
-       part1+= eq[0]
-
-operators.append(Op.Con)
 part2 = 0
 for eq in eqs:
-    if any(recSolve(eq[0], 0, copy(eq[1]), nop) for nop in operators):
-       part2+= eq[0]
+    if any(recSolve(eq[0], 0, eq[1], nop, False) for nop in operators):
+        if not gconUsed:
+            part1 += eq[0]
+        part2 += eq[0]
+    found = False
+    gconUsed = False
 
 print(part1, part2)
-    
