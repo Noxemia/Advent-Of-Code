867b40f8a7a60dc79334d399ccfb45f104df49e0
Aaron Sandgren <aaron.sandgren@live.se>
2024-12-11 10:35:07 +0100

    Solve both

--- 2024/day11/solve.py
+++ 2024/day11/solve.py
@@ -5,22 +5,22 @@ stones = []
 for line in open('input.txt', 'r').readlines():
     stones = [int(x) for x in line.split(" ")]
 
-
 @cache
 def blink(stone: int, blinks: int) -> int:
-    if blinks == 75: return stone
+    if blinks == 0: return 1
 
-    if stone == 0: return blink(1, blinks+1)
+    if stone == 0: return blink(1, blinks-1)
     stonestr: str = str(stone)
     if (sl := len(stonestr)) % 2 == 0:
         lh = stonestr[:(sl//2)]
         rh = stonestr[sl//2:]
-        return blink(int(lh), blinks+1) + blink(int(rh), blinks+1)
+        return blink(int(lh), blinks-1) + blink(int(rh), blinks-1)
     
-    return blink(stone*2024, blinks+1)
+    return blink(stone*2024, blinks-1)
 
-part1 = 0
+part1, part2 = 0, 0
 for stone in stones:
-    part1 += blink(stone, 0)
+    part2 += blink(stone, 75)
+    part1 += blink(stone, 25)
 
-print(part1)
+print(part1, part2)
