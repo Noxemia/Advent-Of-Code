272696c3b3724c8a706a96e5f6bf823621d9ee86
Aaron Sandgren <aaron.sandgren@live.se>
2024-12-19 22:32:52 +0100

    Optimizations

--- 2024/day11/solve.py
+++ 2024/day11/solve.py
@@ -5,27 +5,22 @@ stones = []
 for line in open('input.txt', 'r').readlines():
     stones = [int(x) for x in line.split(" ")]
 
-cache = dict()
+@cache
 def blink(stone: int, blinks: int) -> int:
-    if (stone, blinks) in cache: return cache[(stone,blinks)]
     if blinks == 0: 
-        cache[(stone,blinks)] = 1
         return 1
 
     if stone == 0: 
         res = blink(1, blinks-1)
-        cache[(stone,blinks)] = res
         return res
     stonestr: str = str(stone)
     if (sl := len(stonestr)) % 2 == 0:
         lh = stonestr[:(sl//2)]
         rh = stonestr[sl//2:]
         res = blink(int(lh), blinks-1) + blink(int(rh), blinks-1)
-        cache[(stone,blinks)] = res
         return res
     
     res = blink(stone*2024, blinks-1)
-    cache[(stone,int)] = res
     return res
 
 part1, part2 = 0, 0
